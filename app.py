5c80943c3a01f4b427b21e618a96f24d9606a5ed
S Varsha <varshaadm2020@gmail.com>
2025-10-21 12:41:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,11 +11,11 @@ st.subheader("Pathways of millet PP355677")
 st.markdown("""
     <style>
         .stApp {
-            background-image: url('https://media.istockphoto.com/id/541113276/vector/seamless-vector-pattern-of-germs-and-bacteria.jpg?s=612x612&w=0&k=20&c=M_kNtcWdpMHXNtOe6kxE32NnE3wDvxD7-Jma3ZNXaEs=');  /* Local file path */
+            background-image: url('https://cdn.vectorstock.com/i/1000v/28/30/bacterial-background-thin-line-vector-24482830.jpg');  
             background-size: cover;
             background-position: center;
             background-attachment: fixed;
-            height: 100vh;  /* Full height background */
+          
         }
     </style>
 """, unsafe_allow_html=True)
