0e1f6de3cd60a71da6d00c1b11e57c8a24c039dd
S Varsha <varshaadm2020@gmail.com>
2025-10-21 14:08:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,9 +12,6 @@ st.markdown(
     unsafe_allow_html=True
 )
 
-
-
-
 st.markdown("""
     <style>
         .stApp {
@@ -27,5 +24,47 @@ st.markdown("""
     </style>
 """, unsafe_allow_html=True)
 
+
+def main():
+    col1, col2, col3, col4 = st.columns(4)
+
+    with col1:
+        if st.button("Page 1"):
+            page_1()
+
+    with col2:
+        if st.button("Page 2"):
+            page_2()
+
+    with col3:
+        if st.button("Page 3"):
+            page_3()
+
+    with col4:
+        if st.button("Page 4"):
+            page_4()
+
+
+def page_1():
+    st.subheader("Page 1")
+    st.write("This is content for Page 1.")
+
+# Define content for Page 2
+def page_2():
+    st.subheader("Page 2")
+    st.write("This is content for Page 2.")
+
+# Define content for Page 3
+def page_3():
+    st.subheader("Page 3")
+    st.write("This is content for Page 3.")
+
+# Define content for Page 4
+def page_4():
+    st.subheader("Page 4")
+    st.write("This is content for Page 4.")
+
+if __name__ == "__main__":
+    main()
 
 
