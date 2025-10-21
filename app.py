1d36b79f1fb40be2fd3e09cb40731a48e911558b
S Varsha <varshaadm2020@gmail.com>
2025-10-21 11:50:47 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+st.title("Millet derived Latic Acid Bacteria 16sRNA Analysis")
+
+st.subheader("Pathways of millet PP355677")
+st.dataframe(df)
+
+
