4d4c4d8986235619e3485f31cb4a87d7d020f1a9
S Varsha <varshaadm2020@gmail.com>
2025-10-21 12:49:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,11 +2,11 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
-df=pd.read_csv('pwy_77.csv')
+
 st.title("Millet derived Latic Acid Bacteria 16sRNA Analysis")
 
-st.subheader("Pathways of millet PP355677")
-st.dataframe(df)
+
+
 
 st.markdown("""
     <style>
