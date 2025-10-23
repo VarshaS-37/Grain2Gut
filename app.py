1f1d76f25b62e1b4498c8b2d42d094c7624a4038
S Varsha <varshaadm2020@gmail.com>
2025-10-23 10:32:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -59,8 +59,8 @@ def home():
         1. This app is based on a research paper by our guide, where lactic acid bacteria (LAB) were isolated and characterized from millets([research paper link](https://github.com/VarshaS-37/Grain2Gut/blob/main/Isolation_%26_characterization_of_biological_traits_of_millet-derived_lactic_acid_bacteria.pdf)).
         2. Among the isolates, four LAB strains showed probiotic characteristics, and their 16S rRNA partial sequences were submitted to NCBI.
         3. These sequences have been used for functional prediction using PICRUSt (Phylogenetic Investigation of Communities by Reconstruction of Unobserved States).
-        4. The raw PICRUSt outputs were processed to obtain KO (KEGG Orthology), EC (Enzyme Commission), and PWY (Pathway) datasets.
-        5. Each dataset was independently linked to reference information from databases.
+        4. The raw PICRUSt outputs were processed to obtain KO (KEGG Orthology), EC (Enzyme Commission), and PWY (Pathway) dataframes.
+        5. Each dataframe was independently linked to reference information from databases.
         6. These dataframes are present in the **Meta Data** section and are used for further analysis.
         """)
 
