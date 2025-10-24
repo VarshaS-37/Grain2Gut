721ef27cda9580f1f603c00839f6493a8c2643f6
S Varsha <varshaadm2020@gmail.com>
2025-10-24 12:14:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -443,35 +443,32 @@ def pwy_page():
     if st.button("Back to Home"):
         go_to("home")
 #---------------------------------------------------millet analysis --------------------------------------------------------------------------
-
-
 def millet():
     millet_data = {
         "Millet Source": ["Proso", "Foxtail", "Little", "Little"],
         "Strain": ['BM01', 'NM01', 'SM01', 'SM02'],
-        "Organism": [
-            "Enterococcus casseliflavus", 
-            "Weissella cibaria", 
-            "Weissella cibaria", 
-            "Lactococcus lactis"
-        ],
-        "NCBI Link": [
-            "https://www.ncbi.nlm.nih.gov/nuccore/PP355677.1/", 
+        "Organism": ["Enterococcus casseliflavus", "Weissella cibaria", "Weissella cibaria", "Lactococcus lactis"],
+        "NCBI Link": ["https://www.ncbi.nlm.nih.gov/nuccore/PP355677.1/", 
             "https://www.ncbi.nlm.nih.gov/nuccore/pp355678", 
             "https://www.ncbi.nlm.nih.gov/nuccore/pp355679",
-            "https://www.ncbi.nlm.nih.gov/nuccore/pp355680"
-        ],
+            "https://www.ncbi.nlm.nih.gov/nuccore/pp355680"],
+        "NCBI ID": ['PP355677', 'PP355678', 'PP355679', 'PP355680']
     }
     millet_df = pd.DataFrame(millet_data)
-    st.data_editor(
-        millet_df,
-        column_config={
-            "NCBI Link": st.column_config.LinkColumn("NCBI ID", display_text="Open Link")
-        },
-        hide_index=True,
-        use_container_width=True
-    )
-    left_col, right_col = st.columns([2, 2])  
+    left_col, right_col = st.columns([2, 2]) 
+    with left_col:
+        st.markdown("#### Millet Data")
+        st.data_editor(
+            millet_df,
+            column_config={
+                "NCBI Link": st.column_config.LinkColumn(
+                    "NCBI Link",  # Column label in UI
+                    display_text="Open NCBI Page"  # Text shown for each link
+                ),
+            },
+            hide_index=True,
+            use_container_width=True
+        )
     with right_col:
         st.markdown("#### Explore Analyses")
         col1, col2 = st.columns(2)
@@ -488,8 +485,6 @@ def millet():
         with col4:
             if st.button("Pathway-based Analysis"):
                 go_to("pwy_analysis")
-
-    
 # --------------------------------------------------------------------- Navigation ---------------------------------------------------------------------
 page = st.session_state.page
 if page == "home":
