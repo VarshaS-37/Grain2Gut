ebbce2378e3977335d783abef27a09447057540e
S Varsha <varshaadm2020@gmail.com>
2025-10-27 21:49:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -117,13 +117,13 @@ def home():
     
     # -------------------------------------------------Summarized Analysis-------------------------------------------------------------
     with left_col:
-        for _ in range(6):
+        for _ in range(8):
             st.write("")
         if st.button("Summarized Analysis"):
             go_to("summarized_analysis")
     # ------------------------------------------------Millet-wise Analysis---------------------------------------------------------------
     with middle_col:
-        for _ in range(6):
+        for _ in range(7):
             st.write("")
         if st.button("Millet-wise Analysis"):
             go_to("milletwise_analysis")
