0fcf9e2a899bea3d0535641b5c2fafed11baf5e1
S Varsha <varshaadm2020@gmail.com>
2025-10-27 22:03:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -86,7 +86,7 @@ def go_to(page):
 # ------------------------------------------------------------ Home Page -----------------------------------------------------------------------
 def home():
     st.markdown("<h2 style='text-align:center;'>Grain2Gut</h2>", unsafe_allow_html=True)
-    st.markdown("<h3 style='text-align:center;'><i>Linking genomic potential of Millet derived Lactic Acid Bacteria to food and probiotic applications</i></h3>", unsafe_allow_html=True)
+    st.markdown("<h4 style='text-align:center;'><i>Linking genomic potential of Millet derived Lactic Acid Bacteria to food and probiotic applications</i></h4>", unsafe_allow_html=True)
     st.write("") 
     # ----------------------------------- Sidebar with Project Description ------------------------------------------------------------------------
    # "Back to Home" button at the top of the sidebar
